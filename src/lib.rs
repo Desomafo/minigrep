1556b4175e9865103744b4c6bc4d73d6cc030643
Kamil Kamalov <kamalov-kamil@outlook.com>
2024-10-30 15:19:18 +0900

    Implement case insensitive search

--- src/lib.rs
+++ src/lib.rs
@@ -45,7 +45,16 @@ pub fn search_case_insensitive<'a>(
     query: &str,
     contents: &'a str
 ) -> Vec<&'a str> {
-    vec![]
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
 
 #[cfg(test)]
